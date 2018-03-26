157a76c5a8f04a55c7d1839b246b93ec86f4aa32
Peter Valentine <petervalentine@MacBook-Pro.local>
2018-03-26 11:51:53 +0300

    second try

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -280,5 +280,4 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  rails generate devise:install
 end
